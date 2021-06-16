a6b8997bd0e401b587bf16c8e0316ad8c23f98c0
semeleven <semeleven@yandex-team.ru>
2021-06-17 01:34:18 +0300

    1-login/password

---
+++ server/passport-config.mjs
@@ -0,0 +1,29 @@
+import { Strategy } from "passport-local";
+import bcrypt from "bcrypt";
+
+function initialize(passport, getUserByEmail, getUserById) {
+  const authenticateUser = async (email, password, done) => {
+    const user = getUserByEmail(email);
+    if (user == null) {
+      return done(null, false, { message: "No user with that email" });
+    }
+
+    try {
+      if (await bcrypt.compare(password, user.password)) {
+        return done(null, user);
+      } else {
+        return done(null, false, { message: "Password incorrect" });
+      }
+    } catch (e) {
+      return done(e);
+    }
+  };
+
+  passport.use(new Strategy({ usernameField: "email" }, authenticateUser));
+  passport.serializeUser((user, done) => done(null, user.id));
+  passport.deserializeUser((id, done) => {
+    return done(null, getUserById(id));
+  });
+}
+
+export default initialize;
--- server/server.mjs
+++ server/server.mjs
@@ -1,5 +1,21 @@
 import express from "express";
 import compression from "compression";
+import { urlAlphabet, customAlphabet } from "nanoid";
+import passport from "passport";
+import session from "express-session";
+
+import { Users } from "./db/users.mjs";
+import initializePassport from "./passport-config.mjs";
+
+const nanoid = customAlphabet(urlAlphabet, 25);
+
+const users = new Users();
+
+initializePassport(
+  passport,
+  (email) => users.getByEmail(email),
+  (id) => users.getById(id)
+);
 
 const app = express();
 
@@ -8,14 +24,139 @@ app.use(compression());
 app.use(express.static("./public"));
 app.use(express.static("./build"));
 
-app.set("view-engine", "ejs", );
-app.set('views', './client/views')
+app.set("view-engine", "ejs");
+app.set("views", "./client/views");
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get("/", (req, res) => {
   res.render("index.ejs", { user: req.user });
 });
+
+app.get("/profile", checkAuthenticated, (req, res) => {
+  res.render("profile.ejs");
+});
+
+app.get("/signin", checkNotAuthenticated, (req, res) => {
+  res.render("signin.ejs");
+});
+
+app.get("/signup", checkNotAuthenticated, (req, res) => {
+  res.render("signup.ejs");
+});
+
+app.get("/forgot-password", checkNotAuthenticated, (req, res) => {
+  res.render("forgot-password.ejs");
+});
+
+app.get("/new-password", checkNotAuthenticated, (req, res) => {
+  res.render("new-password.ejs");
+});
+
+const magicLinks = [];
+app.post("/forgot-password", checkNotAuthenticated, (req, res) => {
+  const { email } = req.body;
+
+  const magic = {
+    id: nanoid(),
+    userId: users.getByEmail(email)?.id,
+  };
+  magicLinks.push(magic);
+
+  res.json({ link: "/new-password?magic=" + magic.id });
+});
+
+app.post("/new-password", checkNotAuthenticated, async (req, res) => {
+  const { password } = req.body;
+  const id = req.headers.referer.split("?magic=")[1];
+  const link = magicLinks.find((link) => link.id === id);
+
+  await users.setNewPassword(link.userId, password);
+
+  const user = users.getById(link.userId);
+
+  await login(req, user);
+
+  res.json({
+    success: true,
+  });
+});
+
+app.post("/signin", checkNotAuthenticated, function (req, res, next) {
+  passport.authenticate("local", function (err, user, info) {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res.json({ error: info.message });
+    }
+    req.logIn(user, function (err) {
+      if (err) {
+        return next(err);
+      }
+      return res.json({ success: true });
+    });
+  })(req, res, next);
+});
+
+app.post("/signup", checkNotAuthenticated, async (req, res) => {
+  const { password, name, email } = req.body;
+  try {
+    const user = await users.add({ password, name, email });
+
+    await login(req, user);
+
+    return res.json({
+      success: true,
+    });
+  } catch (e) {
+    return res.json({
+      success: false,
+      error: e.message,
+    });
+  }
+});
+
+app.post("/logout", (req, res) => {
+  req.logOut();
+  res.redirect("/signin");
+});
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+
+  res.redirect("/signin");
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/");
+  }
+  next();
+}
+
+function login(req, user) {
+  return new Promise((resolve, reject) => {
+    req.login(user, function (err) {
+      if (err) {
+        reject(err);
+      }
+      resolve();
+    });
+  });
+}
 
 app.listen(3000, () => {
   console.log("Server ready");
